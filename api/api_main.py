bca745a00bce285fecac53f49893f0cd345a0f6a
lackroy511 <lackroy511@gmail.com>
2024-05-21 07:35:58 +0000

    movies_list

--- api/api_main.py
+++ api/api_main.py
@@ -9,15 +9,28 @@ ES_BASE_URL = 'http://elastic:9200'
 @app.route('/api/movies', methods=['GET'], strict_slashes=False)
 def movies_list() -> str:
     url = f'{ES_BASE_URL}/movies/_search'
-    
+
+    page = request.args.get('page', default=1, type=int)
+    limit = request.args.get('limit', default=50, type=int)
+    sort_by = request.args.get('sort_by', default='id', type=str)
+    sort_order = request.args.get('sort_order', default='asc', type=str)
+    search = request.args.get('search', default='', type=str)
+
     query = {
-        'from': 0,
-        'size': 10,
-        'sort': [
+        'from': (page - 1) * limit,
+        'size': limit,
+        'sort':  [
+            {
+                sort_by: {
+                    'order': sort_order,
+                },
+            },
         ],
-        'query': {
+    }
+    if search:
+        query['query'] = {
             'multi_match': {
-                'query': '',
+                'query': search,
                 'fuzziness': 'auto',
                 'fields': [
                     'title^5',
@@ -27,8 +40,7 @@ def movies_list() -> str:
                     'writers_names^2',
                 ],
             },
-        },
-    }
+        }
 
     response = requests.get(url)
 
